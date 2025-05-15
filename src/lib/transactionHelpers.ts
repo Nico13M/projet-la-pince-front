ea2ca08621e63f83a30f1fc9a22ce92df202c240
Maxence Bonnafoux <maxence.bonnafoux@oclock.school>
2025-05-15 11:25:44 +0200

    FIX: remove transactionHelpers.ts file as it is no longer needed

--- src/lib/transactionHelpers.ts
+++
@@ -1,4 +0,0 @@
-import { Transaction } from '@/types/transaction'
-
-export const calculateTotal = (transactions: Transaction[]) =>
-  transactions.reduce((total, transaction) => total + parseFloat(transaction.montant), 0);
