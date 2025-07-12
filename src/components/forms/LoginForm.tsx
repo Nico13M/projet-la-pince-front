b06b4433ec8029a042d205b2ec2a52ce86048d27
EtiamSani <etiam.sani@laposte.net>
2025-07-12 15:20:25 +0200

    deploy

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -46,6 +46,7 @@ export default function LoginForm() {
       setIsLoading(true)
       const API_LINK = process.env.NEXT_PUBLIC_API_LINK
 
+      
       const response = await fetch(API_LINK + '/auth/signin', {
         method: 'POST',
         headers: {
