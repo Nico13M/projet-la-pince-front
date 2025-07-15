fdcff6348e9c283888693bb6ad24909f1653b3b1
EtiamSani <etiam.sani@laposte.net>
2025-07-15 11:32:50 +0200

    deploy

--- src/app/_actions/user/fetchUser.ts
+++ src/app/_actions/user/fetchUser.ts
@@ -1,24 +1,28 @@
-'use server'
+
 
 import { cookies } from 'next/headers'
-const API_LINK = process.env.API_LINK
+const API_LINK = process.env.NEXT_PUBLIC_API_LINK
 
 export async function fetchUser() {
   try {
-    const cookieStore = await cookies()
-    const csrfToken = cookieStore.get('x-csrf-token')?.value
-    const accessToken = cookieStore.get('access_token')?.value
-    if (!csrfToken) throw new Error('CSRF Token absent')
+   
+    
 
-    const cookieHeader = `x-csrf-token=${csrfToken}; access_token=${accessToken}`
-    const response = await fetch(`${API_LINK}/users/me`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-        'x-csrf-token': csrfToken,
-        Cookie: cookieHeader,
-      },
-    })
+     const cookies = document.cookie.split(';').reduce((acc, cookie) => {
+        const [key, val] = cookie.trim().split('=');
+        acc[key] = val;
+        return acc;
+      }, {} as Record<string, string>);
+      if (!cookies) throw new Error('CSRF Token absent')
+
+      const csrfToken = cookies['XSRF-TOKEN'];
+      const response = await fetch(`${API_LINK}/users/me`, {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+            'x-csrf-token': csrfToken,
+          },
+      })
     return await response.json()
   } catch (error) {
     console.error('Erreur lors du fetch:', error)
