559a0acbc9a376b00a757040d745cb13f5e0a8b1
EtiamSani <etiam.sani@laposte.net>
2025-07-15 11:49:06 +0200

    deploy

--- src/app/_actions/dashboard/fetchUserBudget.ts
+++ src/app/_actions/dashboard/fetchUserBudget.ts
@@ -1,25 +1,23 @@
-'use server'
+
 
 import { cookies } from "next/headers";
 import { Data, SavedBudget } from '@/types/budget'
-const API_LINK = process.env.API_LINK
+const API_LINK = process.env.NEXT_PUBLIC_API_LINK
 export async function fetchUserBudget(pageNumber = 1): Promise<Data<SavedBudget> | undefined> {
     try {
-        const cookieStore = await cookies();
-        const csrfToken = cookieStore.get('x-csrf-token')?.value;
-        const accessToken = cookieStore.get('access_token')?.value;
-        const cookieHeader = `x-csrf-token=${csrfToken}; access_token=${accessToken}`;
+        const cookies = document.cookie.split(';').reduce((acc, cookie) => {
+        const [key, val] = cookie.trim().split('=');
+        acc[key] = val;
+        return acc;
+      }, {} as Record<string, string>);
+      if (!cookies) throw new Error('CSRF Token absent')
 
-        if (!csrfToken) {
-            throw new Error("CSRF Token non trouvé");
-        }
-
+        const csrfToken = cookies['XSRF-TOKEN'];
         const response = await fetch(`${API_LINK}/budget/list?page=${pageNumber}`, {
             method: 'GET',
+            credentials: 'include',
             headers: {
-                'Content-Type': 'application/json',
                 'x-csrf-token': csrfToken,
-                'Cookie': cookieHeader
             }
         })
         const data = await response.json()
@@ -34,21 +32,20 @@ interface CreateBudgetParams {
 }
 export async function createBudget(budget: Partial<SavedBudget>, params: CreateBudgetParams): Promise<SavedBudget | undefined> {
     try {
-        const cookieStore = await cookies();
-        const csrfSecret = cookieStore.get('x-csrf-token')?.value;
-        const csrfToken = cookieStore.get('XSRF-TOKEN')?.value;
-        const accessToken = cookieStore.get('access_token')?.value;
+        const cookies = document.cookie.split(';').reduce((acc, cookie) => {
+        const [key, val] = cookie.trim().split('=');
+        acc[key] = val;
+        return acc;
+      }, {} as Record<string, string>);
+      if (!cookies) throw new Error('CSRF Token absent')
 
-        if (!csrfToken || !accessToken) throw new Error("Tokens manquants");
-
-        const cookieHeader = `x-csrf-token=${csrfSecret}; access_token=${accessToken}`;
+        const csrfToken = cookies['XSRF-TOKEN'];
 
         const response = await fetch(`${API_LINK}/budget/create/${params.categoryId}`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
                 'x-csrf-token': csrfToken,
-                'Cookie': cookieHeader
             },
             body: JSON.stringify({ ...budget, ...params }),
         });
@@ -64,21 +61,22 @@ export async function createBudget(budget: Partial<SavedBudget>, params: CreateB
 
 export async function updateBudget(id: string, budget: Partial<SavedBudget>): Promise<SavedBudget | undefined> {
     try {
-        const cookieStore = await cookies()
-        const csrfSecret = cookieStore.get('x-csrf-token')?.value;
-        const csrfToken = cookieStore.get('XSRF-TOKEN')?.value;
-        const accessToken = cookieStore.get('access_token')?.value;
+        const cookies = document.cookie.split(';').reduce((acc, cookie) => {
+        const [key, val] = cookie.trim().split('=');
+        acc[key] = val;
+        return acc;
+      }, {} as Record<string, string>);
+      if (!cookies) throw new Error('CSRF Token absent')
 
-        if (!csrfToken || !accessToken) throw new Error("Tokens manquants")
+        const csrfToken = cookies['XSRF-TOKEN'];
 
-        const cookieHeader = `x-csrf-token=${csrfSecret}; access_token=${accessToken}`;
+        
 
         const response = await fetch(`${API_LINK}/budget/${id}`, {
             method: 'PATCH',
             headers: {
                 'Content-Type': 'application/json',
                 'x-csrf-token': csrfToken,
-                'Cookie': cookieHeader
             },
             body: JSON.stringify(budget),
         })
@@ -92,21 +90,20 @@ export async function updateBudget(id: string, budget: Partial<SavedBudget>): Pr
 
 export async function deleteBudget(id: string) {
     try {
-        const cookieStore = await cookies()
-        const csrfSecret = cookieStore.get('x-csrf-token')?.value;
-        const csrfToken = cookieStore.get('XSRF-TOKEN')?.value;
-        const accessToken = cookieStore.get('access_token')?.value;
+       const cookies = document.cookie.split(';').reduce((acc, cookie) => {
+        const [key, val] = cookie.trim().split('=');
+        acc[key] = val;
+        return acc;
+      }, {} as Record<string, string>);
+      if (!cookies) throw new Error('CSRF Token absent')
 
-        if (!csrfToken || !accessToken) throw new Error("Tokens manquants")
-
-        const cookieHeader = `x-csrf-token=${csrfSecret}; access_token=${accessToken}`;
+      const csrfToken = cookies['XSRF-TOKEN'];
 
         const response = await fetch(`${API_LINK}/budget/${id}`, {
             method: 'DELETE',
             headers: {
                 'Content-Type': 'application/json',
-                'x-csrf-token': csrfToken,
-                'Cookie': cookieHeader
+                'x-csrf-token': csrfToken
             },
         })
         const data = await response.json()
